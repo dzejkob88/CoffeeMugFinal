7a701bc8569951a3d7c2f69bd13d5a672fa29273
Jakub Noras <jakubnoras@gmail.com>
2020-02-01 23:30:08 +0100

    Final

---
+++ Data/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CoffeeMug.Models;
+
+namespace CoffeeMug.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext (DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoffeeMug.Models
+{
+    public class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
